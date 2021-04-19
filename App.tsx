d68261db616726393b298e664f9327a7676aa334
Breno Alencar <brenoallencs@gmail.com>
2021-04-19 19:48:16 -0300

    First Commit

---
+++ App.tsx
@@ -0,0 +1,11 @@
+import { StatusBar } from 'expo-status-bar';
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+import Welcome from './src/pages/welcome';
+
+export default function App() {
+  return (
+    <Welcome />
+  );
+}
+
